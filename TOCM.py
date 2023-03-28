2eb3d0fa6de23b8c4d2949cdfaca3fddbd913a83
SOH4C4759 <lgy19971027@163.com>
2023-03-28 18:39:01 +0800

    工具完成

--- TOCM.py
+++ TOCM.py
@@ -1,7 +1,5 @@
-import tkinter as tk
-from tkinter import filedialog
-import pandas as pd
-import numpy as np
+from tkinter import filedialog,Tk,Button,Label,Entry
+from pandas import read_excel,isna,Series,ExcelWriter
 
 def add_timecodes(timecode1, timecode2):
     def timecode_to_seconds(timecode):
@@ -27,7 +25,7 @@ def add_timecodes(timecode1, timecode2):
 
 def read_file(file_path, sheet_name):
     if file_path.endswith(('.xlsx', '.xls')):
-        data = pd.read_excel(file_path, sheet_name=sheet_name)
+        data = read_excel(file_path, sheet_name=sheet_name)
     else:
         raise ValueError("Unsupported file format.")
 
@@ -38,23 +36,25 @@ def read_file(file_path, sheet_name):
     for i, value in enumerate(col2_data):
         # print(i,value)
         # 返回第二列的下标与数值
-        if not pd.isna(value):
+        if not isna(value):
             # print(value)
             for j in range(i, length):
                 # print(j)
                 print("时间码：{}".format(col1_data[j]))
                 print("偏移时间：{}".format(str(value)))
                 col1_data[j] = add_timecodes(col1_data[j], str(value))
-    data["最终时间"] = pd.Series(col1_data)
-    data.iloc[:, 6] = pd.Series(col1_data)
+    data["最终时间"] = Series(col1_data)
+    data.iloc[:, 6] = Series(col1_data)
 
-    with pd.ExcelWriter(file_path) as writer:
+    with ExcelWriter(file_path) as writer:
         data.to_excel(writer, sheet_name=sheet_name, index=False)
 
     return col1_data, col2_data,
 
 # D:\Python\Lib\site-packages
 # E:\TimeOffset Calculator for Multilingual\TOCM.py
+# pyinstaller -F -w "E:\TimeOffset Calculator for Multilingual\TOCM.py" -p "D:\Python\Lib\site-packages"
+
 def open_file():
     file_path = filedialog.askopenfilename(title="请选中带时间码的文件",
                                            filetypes=(("Excel Files", "*.xlsx;*.xls"),
@@ -67,41 +67,43 @@ def open_file():
             col1_data, col2_data = read_file(file_path, sheet_name)
             print("Column 1 data:", col1_data)
             print("Column 2 data:", col2_data)
+            from tkinter.messagebox import showinfo
+            showinfo(title="Excel处理已完成",message="Excel文件已计算最终时间，请打开文件确认")
         except Exception as e:
             print(f"Error: {e}")
 
 
 # 创建Tkinter界面
-root = tk.Tk()
+root = Tk()
 root.title("Excel处理")
 
 # 创建按钮，点击后执行open_file函数
-open_button = tk.Button(root, text="打开文件", command=open_file)
+open_button = Button(root, text="打开文件", command=open_file)
 open_button.grid(row=0, column=0, padx=5, pady=5)
 
-sheet_name_label = tk.Label(root, text="Sheet名称")
+sheet_name_label = Label(root, text="Sheet名称")
 sheet_name_label.grid(row=1, column=0, padx=5, pady=5)
 
 # 创建输入框，用于输入Sheet名称
-sheet_name_entry = tk.Entry(root)
+sheet_name_entry = Entry(root)
 sheet_name_entry.grid(row=2, column=0, padx=5, pady=5)
 
 
 # 创建标签，显示“第一列名称”
-col1_name_label = tk.Label(root, text="第一列名称")
+col1_name_label = Label(root, text="第一列名称")
 col1_name_label.grid(row=3, column=0, padx=5, pady=5)
 
 # 创建输入框，用于输入第一列名称
-col1_name_entry = tk.Entry(root)
+col1_name_entry = Entry(root)
 col1_name_entry.grid(row=4, column=0, padx=5, pady=5)
 col1_name_entry.insert(0, "时间码")  # 默认值
 
 # 创建标签，显示“第二列名称”
-col2_name_label = tk.Label(root, text="第二列名称")
+col2_name_label = Label(root, text="第二列名称")
 col2_name_label.grid(row=5, column=0, padx=5, pady=5)
 
 # 创建输入框，用于输入第二列名称
-col2_name_entry = tk.Entry(root)
+col2_name_entry = Entry(root)
 col2_name_entry.grid(row=6, column=0, padx=5, pady=5)
 col2_name_entry.insert(0, "时间偏差")  # 默认值
 
